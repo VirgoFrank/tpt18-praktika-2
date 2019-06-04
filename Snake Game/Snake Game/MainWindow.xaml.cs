baa07b3ffa5a4a5e5d8a878b4871551836ab1d8f
JasperNoorkoiv <jaspernoork@gmail.com>
2019-06-04 14:04:06 +0300

    teine päev

--- Snake Game/Snake Game/MainWindow.xaml.cs
+++ Snake Game/Snake Game/MainWindow.xaml.cs
@@ -20,62 +20,132 @@ namespace Snake_Game
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        Random rnd = new Random();
+        Rectangle tile = new Rectangle();
+        double cellsize = 50;
+        double cellcount = 16;
+       
+       
+
         public MainWindow()
         {
             InitializeComponent();
+           Drawboard();
+          
+            Canvas.SetLeft(RISTKÜLIK, 0);
+            Canvas.SetTop(RISTKÜLIK, 0);
+        }
+        public void Drawboard()
+        {
+            for (int j = 0; j < cellcount; j++)
+            {
+
+                for (int i = 0; i < cellcount; i++)
+                {
+                    if (i % 2 == 0 && j % 2 == 0 || i % 2 == 1 && j % 2 == 1)
+                    {
+                        Rectangle tile = new Rectangle();
+                        tile.Height = cellsize;
+                        tile.Width = cellsize;
+                        tile.Fill = Brushes.Lime;
+                        Canvas.SetTop(tile, j * cellsize);
+                        Canvas.SetLeft(tile, i * cellsize);
+                        canvas.Children.Add(tile);
+
+                    }
+                }
+            }
+        }
+
+        public void toit()
+        {
+            Ellipse söök = new Ellipse();
+
+
+            söök.Width = cellsize;
+            söök.Height = cellsize;
+            söök.Fill = Brushes.Red;
+            int top = rnd.Next(8);
+            int left = rnd.Next(16);
+            Canvas.SetTop(söök, top*50);
+            Canvas.SetLeft(söök, left*50);
+        
+            canvas.Children.Add(söök);
+          
+
         }
 
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
+           
+           
             if (e.LeftButton == MouseButtonState.Pressed)
             {
-                double currentleft = Canvas.GetLeft(RISTKÜLIK);
-                double newleft = currentleft + 20;
-                Canvas.SetLeft(RISTKÜLIK, newleft);
+                toit();
+               
             }
 
-            if (e.RightButton == MouseButtonState.Pressed)
-            {
-                double currentleft = Canvas.GetLeft(RISTKÜLIK);
-                double newleft = currentleft - 20;
-                Canvas.SetLeft(RISTKÜLIK, newleft);
-            }
-            if (e.MiddleButton == MouseButtonState.Pressed)
-            {
-                double currentleft = Canvas.GetTop(RISTKÜLIK);
-                double newleft = currentleft + 20;
-                Canvas.SetTop(RISTKÜLIK, newleft);
-            }
+           
 
         }
+        private void Movesnake(bool up, bool down, bool right, bool left)
+        {
+
+
+            if (up || down)
+            {
+                double currentTop = Canvas.GetTop(RISTKÜLIK);
+                double newtop = up
+                    ? currentTop - cellsize
+                    : currentTop + cellsize;
+                Canvas.SetTop(RISTKÜLIK, newtop);
+            }
+            if (left || right)
+            {
+                double currentLeft = Canvas.GetLeft(RISTKÜLIK);
+                double newLeft = left
+                    ? currentLeft + cellsize
+                    : currentLeft - cellsize;
+                Canvas.SetLeft(RISTKÜLIK, newLeft);
+            }
+        }
+
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
-            if ( e.Key == Key.Right ||e.Key == Key.D)
-            {
-                double currentleft = Canvas.GetLeft(RISTKÜLIK);
-                double newleft = currentleft + 20;
-                Canvas.SetLeft(RISTKÜLIK, newleft);
-            }
-            if (e.Key == Key.Left || e.Key == Key.A)
-            {
-                double currentleft = Canvas.GetLeft(RISTKÜLIK);
-                double newleft = currentleft - 20;
-                Canvas.SetLeft(RISTKÜLIK, newleft);
-            }
-            if (e.Key == Key.Up || e.Key == Key.W)
-            {
-                double currentleft = Canvas.GetTop(RISTKÜLIK);
-                double newleft = currentleft - 20;
-                Canvas.SetTop(RISTKÜLIK, newleft);
-            }
-            if (e.Key == Key.Down || e.Key == Key.S)
-            {
-                double currentleft = Canvas.GetTop(RISTKÜLIK);
-                double newleft = currentleft + 20;
-                Canvas.SetTop(RISTKÜLIK, newleft);
-            }
 
+            bool up = e.Key == Key.Up;
+            bool down = e.Key == Key.Down;
+            bool right = e.Key == Key.Right;
+            bool left = e.Key == Key.Left;
+
+            Movesnake(up, down, left, right);
+            /*
+            if (Canvas.GetTop(RISTKÜLIK) < 0 || Canvas.GetTop(RISTKÜLIK) > 400 || Canvas.GetLeft(RISTKÜLIK) > 750 || Canvas.GetLeft(RISTKÜLIK) < 0 || e.Key == Key.R)
+             {
+                 Canvas.SetLeft(RISTKÜLIK, 0);
+                 Canvas.SetTop(RISTKÜLIK, 0);
+
+             }
+            */
+
+            if (Canvas.GetTop(RISTKÜLIK) < 0)
+            {
+                Canvas.SetTop(RISTKÜLIK, 400);
+            }
+            if (Canvas.GetTop(RISTKÜLIK) > 400)
+            {
+                Canvas.SetTop(RISTKÜLIK, 0);
+            }
+            if (Canvas.GetLeft(RISTKÜLIK) < 0)
+            {
+                Canvas.SetLeft(RISTKÜLIK, 750);
+            }
+            if (Canvas.GetLeft(RISTKÜLIK) > 750)
+            {
+                Canvas.SetLeft(RISTKÜLIK, 0);
+            }
         }
     }
 }
