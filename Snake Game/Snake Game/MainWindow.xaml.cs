d023ac32f520bc6df974615f81b14480a9fa4385
JasperNoorkoiv <jaspernoork@gmail.com>
2019-06-05 13:55:41 +0300

    3 päev kõik on katki

--- Snake Game/Snake Game/MainWindow.xaml.cs
+++ Snake Game/Snake Game/MainWindow.xaml.cs
@@ -1,174 +1,126 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
-namespace Snake_Game
+namespace SnakeGame
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-
-        Random rnd = new Random();
-        Rectangle tile = new Rectangle();
-        double cellsize = 50;
-        double cellcount = 16;
-       
-       
+        const double CellSize = 30D;
+        const int CellCount = 16;
+        Ellipse söök = new Ellipse();
+        DispatcherTimer timer;
+        Snake snake;
+        
 
         public MainWindow()
         {
             InitializeComponent();
-           Drawboard();
-          
-            int top = rnd.Next(8);
-            int left = rnd.Next(16);
-            toit(top, left);
-            Canvas.SetLeft(RISTKÜLIK, 0);
-            Canvas.SetTop(RISTKÜLIK, 0);
+            Drawboard();
+            snake = new Snake(snakeShape, CellSize, CellCount);
+
+            snake.Init();
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(2);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+
         }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
         public void Drawboard()
         {
-            for (int j = 0; j < cellcount; j++)
+            for (int j = 0; j < CellCount; j++)
             {
 
-                for (int i = 0; i < cellcount; i++)
+                for (int i = 0; i < CellCount; i++)
                 {
                     if (i % 2 == 0 && j % 2 == 0 || i % 2 == 1 && j % 2 == 1)
                     {
                         Rectangle tile = new Rectangle();
-                        tile.Height = cellsize;
-                        tile.Width = cellsize;
+                        tile.Height = CellSize;
+                        tile.Width = CellSize;
                         tile.Fill = Brushes.Lime;
-                        Canvas.SetTop(tile, j * cellsize);
-                        Canvas.SetLeft(tile, i * cellsize);
-                        canvas.Children.Add(tile);
+                        Canvas.SetTop(tile, j * CellSize);
+                        Canvas.SetLeft(tile, i * CellSize);
+                         board.Children.Add(tile);
 
                     }
                 }
             }
         }
-
-       public int toit(int top, int left)
+        /*
+        private void toit()
         {
-            Ellipse söök = new Ellipse();
+
 
 
             söök.Width = cellsize;
             söök.Height = cellsize;
             söök.Fill = Brushes.Red;
-           // int top = rnd.Next(8);
-          //  int left = rnd.Next(16);
-            Canvas.SetTop(söök, top*50);
-            Canvas.SetLeft(söök, left*50);
+
+            int top = rnd.Next(8);
+            int left = rnd.Next(16);
+            Canvas.SetTop(söök, top * 50);
+            Canvas.SetLeft(söök, left * 50);
             int k = top * 50;
-        
+
             canvas.Children.Add(söök);
-            return top;
-           
+
+
 
         }
+        */
 
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        //private void DirectSnake(Direction direction)
+        //{
+        //    snakeDirection = direction;
+        //    lblSnakeDirection.Content =
+        //        $"Direction: {direction}";
+        //}
+
+
+        private void Timer_Tick(object sender, EventArgs e)
         {
-           
-           
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                //toit();
-               
-            }
-
-           
-
-        }
-        private void Movesnake(direction direction)
-        {
-
-
-            if (direction == direction.up || direction == direction.down)
-            {
-                double currentTop = Canvas.GetTop(RISTKÜLIK);
-                double newtop = direction == direction.up
-                    ? currentTop - cellsize
-                    : currentTop + cellsize;
-                Canvas.SetTop(RISTKÜLIK, newtop);
-            }
-            if (direction == direction.left || direction == direction.right)
-            {
-                double currentLeft = Canvas.GetLeft(RISTKÜLIK);
-                double newLeft = direction ==direction.right
-                    ? currentLeft + cellsize
-                    : currentLeft - cellsize;
-                Canvas.SetLeft(RISTKÜLIK, newLeft);
-            }
+            snake.Move();
         }
 
-
-        private void Window_KeyDown(object sender, KeyEventArgs e)
+        private void Window_KeyDown(
+            object sender, KeyEventArgs e)
         {
-            direction direction;
-            if (e.Key == Key.Up || e.Key == Key.W)
-                direction = direction.up;
-            else if (e.Key == Key.Down || e.Key == Key.S)
-                direction = direction.down;
-            else if (e.Key == Key.Left || e.Key == Key.A)
-                direction = direction.left;
-            else if (e.Key == Key.Right || e.Key == Key.D)
-                direction = direction.right;
-            else
-                return;
-            Movesnake(direction);
-
-            Ellipse söök = new Ellipse();
-
-
-            // if (Canvas.GetLeft(RISTKÜLIK) == )
-
-            /*
-            if (Canvas.GetTop(RISTKÜLIK) < 0 || Canvas.GetTop(RISTKÜLIK) > 400 || Canvas.GetLeft(RISTKÜLIK) > 750 || Canvas.GetLeft(RISTKÜLIK) < 0 || e.Key == Key.R)
-             {
-                 Canvas.SetLeft(RISTKÜLIK, 0);
-                 Canvas.SetTop(RISTKÜLIK, 0);
-
-             }
-            */
-
-            if (Canvas.GetTop(RISTKÜLIK) < 0)
+            Direction direction;
+            switch (e.Key)
             {
-                Canvas.SetTop(RISTKÜLIK, 400);
+                case Key.Up:
+                    direction = Direction.Up;
+                    break;
+                case Key.Down:
+                    direction = Direction.Down;
+                    break;
+                case Key.Left:
+                    direction = Direction.Left;
+                    break;
+                case Key.Right:
+                    direction = Direction.Right;
+                    break;
+                default:
+                    return;
             }
-            if (Canvas.GetTop(RISTKÜLIK) > 400)
-            {
-                Canvas.SetTop(RISTKÜLIK, 0);
-            }
-            if (Canvas.GetLeft(RISTKÜLIK) < 0)
-            {
-                Canvas.SetLeft(RISTKÜLIK, 750);
-            }
-            if (Canvas.GetLeft(RISTKÜLIK) > 750)
-            {
-                Canvas.SetLeft(RISTKÜLIK, 0);
-            }
-        }
-        public enum direction
-        {
-            up,
-            down,
-            left,
-            right
+
+            snake.ChangeDirection(direction);
         }
     }
 }
