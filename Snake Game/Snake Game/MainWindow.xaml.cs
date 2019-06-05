9192a84fe2d8a246d1b8bc1de97d4da21e07147f
Virgo Frank <Virgo.Frank@tptlive.ee>
2019-06-05 17:09:22 +0300

    Kodutöö Classid ei tööta.

--- Snake Game/Snake Game/MainWindow.xaml.cs
+++ Snake Game/Snake Game/MainWindow.xaml.cs
@@ -1,30 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 
-namespace SnakeGame
+namespace Snake_Game
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        const double CellSize = 30D;
-        const int CellCount = 16;
+
+        Random rnd = new Random();
+        Rectangle tile = new Rectangle();
+        double cellsize = 50;
+        double cellcount = 16;
         Ellipse söök = new Ellipse();
         DispatcherTimer timer;
-        Snake snake;
-        
+        Snake snake = new Snake; // millegi pärast visual studio ei leia neid classe üles
 
         public MainWindow()
         {
             InitializeComponent();
             Drawboard();
-            snake = new Snake(snakeShape, CellSize, CellCount);
+            snake = new Snake(RISTKÜLIK, cellsize, cellcount);
 
             snake.Init();
 
@@ -33,30 +43,30 @@ namespace SnakeGame
             timer.Tick += Timer_Tick;
             timer.Start();
 
-
+            Canvas.SetLeft(RISTKÜLIK, 0);
+            Canvas.SetTop(RISTKÜLIK, 0);
         }
-
-        private void InitializeComponent()
+        private void Timer_Tick(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            snake.Move();
         }
 
         public void Drawboard()
         {
-            for (int j = 0; j < CellCount; j++)
+            for (int j = 0; j < cellcount; j++)
             {
 
-                for (int i = 0; i < CellCount; i++)
+                for (int i = 0; i < cellcount; i++)
                 {
                     if (i % 2 == 0 && j % 2 == 0 || i % 2 == 1 && j % 2 == 1)
                     {
                         Rectangle tile = new Rectangle();
-                        tile.Height = CellSize;
-                        tile.Width = CellSize;
+                        tile.Height = cellsize;
+                        tile.Width = cellsize;
                         tile.Fill = Brushes.Lime;
-                        Canvas.SetTop(tile, j * CellSize);
-                        Canvas.SetLeft(tile, i * CellSize);
-                         board.Children.Add(tile);
+                        Canvas.SetTop(tile, j * cellsize);
+                        Canvas.SetLeft(tile, i * cellsize);
+                        canvas.Children.Add(tile);
 
                     }
                 }
@@ -85,21 +95,9 @@ namespace SnakeGame
         }
         */
 
-        //private void DirectSnake(Direction direction)
-        //{
-        //    snakeDirection = direction;
-        //    lblSnakeDirection.Content =
-        //        $"Direction: {direction}";
-        //}
 
 
-        private void Timer_Tick(object sender, EventArgs e)
-        {
-            snake.Move();
-        }
-
-        private void Window_KeyDown(
-            object sender, KeyEventArgs e)
+        private void Window_KeyDown(object sender, KeyEventArgs e)
         {
             Direction direction;
             switch (e.Key)
@@ -117,10 +115,53 @@ namespace SnakeGame
                     direction = Direction.Right;
                     break;
                 default:
-                    return;
-            }
 
-            snake.ChangeDirection(direction);
+/*---kodutöö---->*/ if (Canvas.GetTop(RISTKÜLIK) < 0 || Canvas.GetTop(RISTKÜLIK) > 400 || Canvas.GetLeft(RISTKÜLIK) > 750 
+                        || Canvas.GetLeft(RISTKÜLIK) < 0 || e.Key == Key.R) // Saadab ussi vasakule üless nurka
+                    {
+                        Canvas.SetLeft(RISTKÜLIK, 0);
+                        Canvas.SetTop(RISTKÜLIK, 10);
+
+                    }
+                    return;
+                    /*
+                    if (Canvas.GetLeft(RISTKÜLIK) == Canvas.GetLeft(söök) && Canvas.GetTop(RISTKÜLIK) == Canvas.GetTop(söök))
+                        söök.Fill = Brushes.Black;
+
+
+                    // if (Canvas.GetLeft(RISTKÜLIK) == )
+                    */
+                    
+                  
+                    
+                    /*
+                    if (Canvas.GetTop(RISTKÜLIK) < 0)
+                    {
+                        Canvas.SetTop(RISTKÜLIK, 400);
+                    }
+                    if (Canvas.GetTop(RISTKÜLIK) > 400)
+                    {
+                        Canvas.SetTop(RISTKÜLIK, 0);
+                    }
+                    if (Canvas.GetLeft(RISTKÜLIK) < 0)
+                    {
+                        Canvas.SetLeft(RISTKÜLIK, 750);
+                    }
+                    if (Canvas.GetLeft(RISTKÜLIK) > 750)
+                    {
+                        Canvas.SetLeft(RISTKÜLIK, 0);
+                    }*/
+            }
         }
+
+        public enum direction
+        {
+            up,
+            down,
+            left,
+            right
+        }
+
+       
     }
 }
